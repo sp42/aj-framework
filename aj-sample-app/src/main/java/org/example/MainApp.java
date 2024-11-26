eaf39b8c7c21030124eda7376fe151222f09079d
sp42 <sp42@qq.com>
2024-11-26 15:56:22 +0800

    ci

--- aj-sample-app/src/main/java/org/example/MainApp.java
+++ aj-sample-app/src/main/java/org/example/MainApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class MainApp {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(MainApp.class, args);
         System.out.printf("Hello and welcome!");
     }
 }
